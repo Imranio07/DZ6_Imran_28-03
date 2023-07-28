525a2ea662482b6a5fe1ed6757f3057016fa0f17
Imranio07 <kurutkg09@gmail.com>
2023-07-28 17:38:14 +0600

    first commit

--- app.js
+++ app.js
@@ -1,40 +1,13 @@
-// // контекст this
-// let human = {
-//     name: 'John',
-//     age: 23,
-//     c: undefined,
-//     isMarried: true,
-//     showInfo(){
-//         // console.log(`${human.name} ${human.age}`)
-//         function info(){
-//             console.log(`${this.name} ${this.age}`)
-//         }
-//         info()
-//     }
-// }
-// human.showInfo()
-//
-// //default-params
-// let array = [1, 2, 3, 4, 5]
-// let newArray = array.join()
-// console.log(newArray)
-//
-// function pushJoin(array, separator = ','){
-//     let result = ''
-//     for(let i = 0; i < array.length; i++){
-//         result += array[i] + (i === array.length - 1 ? '': separator)
-//     }
-//     return result
-// }
-//
-// console.log(pushJoin([1, 23],';'));
-
-
 const input = document.querySelector('#input')
 const createButton = document.querySelector('#create_button')
 const todoList = document.querySelector('#todo_list')
-
 const createTodo = () => {
+    const inputValue = input.value.trim()
+    if (inputValue.length === 0) {
+        alert('пустоту нельзя добавлять');
+        return;
+    }
+
     const div = document.createElement('div')
     const text = document.createElement('h3')
     const divButtons = document.createElement('div')
@@ -63,18 +36,12 @@ const createTodo = () => {
 
     input.value = ''
 
-}
+    text.addEventListener('click', () => {
+        text.classList.toggle('strike-through');
+    });
 
+}
 createButton.onclick = () => createTodo()
-// window.addEventListener('keydown', (event) =>{
-//     if(event.code === 'Enter'){
-//         createTodo()
-//     }
-// })
 
-window.onkeydown = (event) => {
-    // console.log(event.keyCode)
-    if (event.keyCode === 13) {
-        createTodo()
-    }
-}
+
+
